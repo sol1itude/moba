3857bb12ab76315b22842e0f156911fee9bd8f92
e <wangfengjie123@gmail.com>
2020-07-31 16:59:12 +0800

    添加部分文件

---
+++ server/index.js
@@ -0,0 +1,12 @@
+const express = require('express');
+
+const app = express();
+
+app.use(require('cors')());
+app.use(express.json());
+
+require('./routes/admin/index.js')(app)
+require('./plugins/db.js')(app)
+app.listen(3000, () => {
+    console.log('localhost://3000')
+})
