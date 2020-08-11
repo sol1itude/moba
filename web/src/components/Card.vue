1c4ca8d0aa913b65fdcd3790873f68e5f6d5ab06
e <wangfengjie123@gmail.com>
2020-08-11 17:23:52 +0800

    更新

---
+++ web/src/components/Card.vue
@@ -0,0 +1,51 @@
+<template>
+    <div class="card">
+        <div class="title">{{title}}</div>
+        <div class="tab">
+            <p @click="activeIndex = index" :class="{active: activeIndex === index}" v-for="(cat, index) in cats" :key="index">{{cat.name}}</p>
+        </div>
+        <div class="cont">
+            <div v-for="(cat, index) in cats" :key="index" class="citem">
+
+                <slot name="items" :ccc="cat"></slot>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    props: {
+        title: {type: String, required: true},
+        cats: {type: Array, required: true}
+    },
+    data(){
+        return {
+            activeIndex: 0
+        }
+    }
+}
+</script>
+
+<style lang="scss" scoped>
+    .card{
+      background-color: #fff;
+      padding: 40px;
+      .title{
+        font-weight: 900;
+        font-size: 32px;
+      }
+      .cont{
+        margin-top: 20px;
+      }
+      .tab{
+          display: flex;
+          justify-content: space-between;
+          font-size: 28px;
+          margin-top: 20px;
+          .active{
+              color: red;
+          }
+      }
+    }
+</style>
--- web/src/views/Home.vue
+++ web/src/views/Home.vue
@@ -1,18 +1,65 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <Card title="新闻" :cats="list">
+      <template #items="{ccc}">
+        <div v-for="(item, index) in ccc.l" :key="index">
+          {{item.title}}
+        </div>
+      </template>
+    </Card>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
-
+import Card from '../components/Card.vue'
 export default {
-  name: 'Home',
   components: {
-    HelloWorld
+    Card
+  },
+  data(){
+    return {
+      list: [
+        {
+          name: '新闻',
+          l: [
+            {
+              title: '111',
+              cont: '222'
+            },
+            {
+              title: '123',
+              cont: '212322'
+            },
+            {
+              title: '123',
+              cont: '123'
+            }
+          ]
+        },
+        {
+          name: '视频',
+          l: [{
+            title: '222',
+            cont: '222'
+          }]
+        },
+        {
+          name: '资讯',
+          l: [{
+            title: '333',
+            cont: '222'
+          }]
+        }
+      ]
+    }
   }
 }
 </script>
+
+<style lang="scss">
+  html{
+    height: 100%;
+    background-color: #f8f8f8;
+  }
+
+</style>
